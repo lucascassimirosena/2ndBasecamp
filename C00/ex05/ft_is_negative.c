bdf54eb0899232520e8474d831ca2e118cafab6c
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-11 21:20:54 -0300

    solving

---
+++ C00/ex05/ft_is_negative.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucassim <lucassim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 17:02:48 by lucassim          #+#    #+#             */
+/*   Updated: 2022/04/09 15:45:20 by lucassim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Criado por lucassim
+
+#include <unistd.h>
+
+void ft_print_comb(void)
+{
+    char    seven;
+    char    eight;
+    char    nine;
+
+    seven = '0';
+    
+}
