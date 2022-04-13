1a99b20c2f4a1c020a3bb89212541f5e51c1875b
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-13 16:48:30 -0300

    updated

--- C/C01/ex00/00Comentarios.c
+++ C/C01/ex00/00Comentarios.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lucassim <lucassim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/08 17:02:48 by lucassim          #+#    #+#             */
-/*   Updated: 2022/04/09 15:45:20 by lucassim         ###   ########.fr       */
+/*   Created: 2022/04/13 01:47:48 by lucassim          #+#    #+#             */
+/*   Updated: 2022/04/13 01:58:32 by lucassim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
+O exercício pede para escrevermos uma função que tenha um ponteiro para int em parâmetro
+e dê ao int o valor de 42.
+
+ela deve ser prototipada dessa maneira:
+void    ft_ft(int *nbr);
 
 
 
 
 */
 
-// Criado por lucassim
-
-#include <unistd.h>
-
+void    ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
