f3fb63605e51cb6ff60c34639b91e25dcba9b073
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-13 17:22:19 -0300

    added comments

---
+++ C/C01/ex01/00Comments.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucassim <lucassim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 01:47:48 by lucassim          #+#    #+#             */
+/*   Updated: 2022/04/13 01:58:32 by lucassim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Este exercícoi pede para que possamos criar um ponteiro que aponte para um ponteiro
+que aponte para um ponteiro que aponte para um ponteiro que aponte para um ponteiro
+que aponte para um ponteiro que aponte para um ponteiro que aponte para um ponteiro
+que aponte para um ponteiro e que este tenha o valor 42 nele.
+
+faremos a mesma coiisa mas apontando o ponteiro do ponteiro do ponteiro do ponteiro
+do ponteiro do ponteiro do ponteiro do ponteiro do ponteiro.
+
+*/
+
+void    ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
