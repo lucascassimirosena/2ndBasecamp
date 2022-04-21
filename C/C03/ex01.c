10b5d2315ef102311f9bb21b5a4a9215f714f90a
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-21 16:34:25 -0300

    ok this time I don't remember what I changed

--- C/C03/ex01.c
+++ C/C03/ex01.c
@@ -13,28 +13,13 @@
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
-	int				a;
-	int				b;
-	int				c;
-	int				d;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] != s2[i])
-		{
-			a = s1[i];
-			b = s2[i];
-			return (a - b);
-		}
+			return (s1[i] - s2[i]);
 		i++;
-	}
-	if ((i < n && s2[i] != '\0' && s1[i] == '\0')
-		|| (s1[i] != '\0' && s2[i] == '\0'))
-	{
-		c = s1[i];
-		d = s2[i];
-		return (c - d);
 	}
 	return (0);
 }
