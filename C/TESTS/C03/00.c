6a878c95315bd6cf0d1415241ab3e60b3b689ac4
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-04-21 13:37:33 -0300

    updated the tests

--- C/TESTS/C03/00.c
+++ C/TESTS/C03/00.c
@@ -1,46 +1,41 @@
 // Este teste foi criado por uma pessoa da minha vila
 // Criarei meus próprios posteriormente
 
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(void)
+int	ft_strcmp(char *s1, char *s2);
+
+int	main(void)
 {
-    char *s1="teste";
-    char *s2="teste"; // igual s1
-    char *s3="tEste";
-    char *s4="t3ste";
-    char *s5="te$te";
-    char s6[]={-10, 'e', 's', 't', 'e'};
-    char *s7="testee";
-    char *s8="test";
+	char *s1;
+	char *s2;
+	char *s3;
+	char *s4;
+	char s5[] = {'a', 'b', 'c', -10, -20, '\0'};
+	char *s6;
+	char *s7;
 
-    printf("s1: %s, s2: %s, s3: %s, s4: %s, s5: %s, s6: %s, s7: %s, s8: %s\n\n", s1, s2, s3, s4, s5, s6, s7, s8);
-    printf("comparando s1 com s2\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s2,7));
-    printf("função original= %d\n\n", strncmp(s1, s2,7));
-
-    printf("comparando s1 com s3\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s3,7));
-    printf("função original= %d\n\n", strncmp(s1, s3,7));
-
-    printf("comparando s1 com s4\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s4,7));
-    printf("função original= %d\n\n", strncmp(s1, s4,7));
-
-    printf("comparando s1 com s5\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s5,7));
-    printf("função original= %d\n\n", strncmp(s1, s5,7));
-
-    printf("comparando s1 com s6\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s6,7));
-    printf("função original= %d\n\n", strncmp(s1, s6,7));
-
-    printf("comparando s1 com s7\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s7,7));
-    printf("função original= %d\n\n", strncmp(s1, s7,7));
-
-    printf("comparando s1 com s8\n");
-    printf("ft_strcmp = %d\n", ft_strncmp(s1, s8,7));
-    printf("função original= %d\n\n", strncmp(s1, s8,7));
+	s1 = calloc(10, sizeof(char));
+	s2 = calloc(8, sizeof(char));
+	s3 = calloc(10, sizeof(char));
+	s4 = calloc(10, sizeof(char));
+	s6 = calloc(10, sizeof(char));
+	s7 = calloc(1, sizeof(char));
+	s1 = "abcdefghi";
+	s2 = "abcdefg";
+	s3 = "abcdEfghi";
+	s4 = "ab0defghi";
+	s6 = "abcdefghi"; // s6 is the same of s1
+	s7 = "";
+	printf("s1: %s, s2: %s, s3: %s, s4: %s, s5: %s, s6: %s, s7: %s\n", s1, s2, s3, s4, s5, s6, s7);
+	printf("ft_strcmp(s1, s2)\t= %d\noriginal strcmp(s1, s2)\t= %d\n------------------------------\n", ft_strcmp(s1, s2), strcmp(s1, s2));
+	printf("ft_strcmp(s1, s3)\t= %d\noriginal strcmp(s1, s3)\t= %d\n------------------------------\n", ft_strcmp(s1, s3), strcmp(s1, s3));
+	printf("ft_strcmp(s1, s4)\t= %d\noriginal strcmp(s1, s4)\t= %d\n------------------------------\n", ft_strcmp(s1, s4), strcmp(s1, s4));
+	printf("ft_strcmp(s1, s5)\t= %d\noriginal strcmp(s1, s5)\t= %d\n------------------------------\n", ft_strcmp(s1, s5), strcmp(s1, s5));
+	printf("ft_strcmp(s1, s6)\t= %d\noriginal strcmp(s1, s6)\t= %d\n------------------------------\n", ft_strcmp(s1, s6), strcmp(s1, s6));
+	printf("ft_strcmp(s1, s7)\t= %d\noriginal strcmp(s1, s7)\t= %d\n------------------------------\n", ft_strcmp(s1, s7), strcmp(s1, s7));
+	return(0);
 }
--- C/TESTS/C03/02.c
+++ C/TESTS/C03/02.c
@@ -1,15 +1,33 @@
-// Este teste foi criado por uma pessoa da minha vila
-// Criarei meus próprios posteriormente
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(void)
-{
-    char s1[17]="teste de ";
-    char s2[7]="string";
+char	*ft_strcat(char *dest, char *src);
 
-    printf("s1: %s, s2: %s\n", s1, s2);
-    printf("função original= %s\n\n", strcat(s1, s2));
-    printf("ft_strcat = %s\n", ft_strcat(s1, s2));
+int	main(void)
+{
+	char *src;
+	char *dest;
+	char *ft_src;
+	char *ft_dest;
+	char *result;
+	char *ft_result;
+
+	src = calloc(7, sizeof(char));
+	dest = calloc(12, sizeof(char));
+	ft_src = calloc(7, sizeof(char));
+	ft_dest = calloc(12, sizeof(char));
+	strcpy(src, "galera");
+	strcpy(dest, "alo ");
+	strcpy(ft_src, "galera");
+	strcpy(ft_dest, "alo ");
+	result = strcat(dest, src);
+	ft_result = ft_strcat(ft_dest, ft_src);
+	if (ft_dest != ft_result)
+		printf("KO, return value of ft_strcar is different of ft_dest. ft_strcar=%p ft_dest=%p\n", ft_result, ft_dest);
+	else if (strcmp(result, ft_result) != 0)
+		printf("KO, value returned from strcmp with ft_strcat and strcat is different of zero(%d).\n", strcmp(result, ft_result));
+	else
+		printf("OK.\nresult: %s\n", ft_result);
+	return (0);
 }
